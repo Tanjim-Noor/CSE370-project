a4f628cdedc0867acbd86a6d5945097bee184093
Tanjim-Noor <128420437+Tanjim-Noor@users.noreply.github.com>
2023-03-21 19:07:42 +0000

    some changes to templates

--- main.py
+++ main.py
@@ -2,9 +2,20 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-  return render_template('base.html')
+
+@app.route('/') #home
+def home():
+  return render_template('home.html')
+
+
+@app.route('/signup') #signup
+def sign_up():
+  return render_template('signup.html')
+
+@app.route('/shows')
+def shows():
+  return render_template('shows.html')
+
 
 if __name__ == '__main__':
-  app.run(host = "0.0.0.0", debug=True)
+  app.run(host="0.0.0.0", debug=True)
